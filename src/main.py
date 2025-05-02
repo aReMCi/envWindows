d2be69b05a4cd093277fd0c0ce48f3bfa33adbaf
Richard <richard1602cuellar@gmail.com>
2025-05-01 23:23:59 -0400

    Expandiendo

--- src/main.py
+++ src/main.py
@@ -6,7 +6,10 @@ from models.model import DataModel as Model
 def main():
     root = tk.Tk()
     root.title("Tkinter MVC App")
-    
+    root.geometry("600x400")
+    root.iconbitmap("logo.bmp")
+    root.config(bg="lightblue")
+
     model = Model()
     model.add_data("Sample Data option 1")
     view = View(root)
